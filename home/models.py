703e070ee73423117b42aac899604e0b8893b021
haley <78947754+haleyalwayshappy@users.noreply.github.com>
2024-05-08 03:23:03 +0900

    디자인 적용, 모달창 추가
    홈화면에 디자인 적용
    모달창 추가 > 동영상 재생 추가

--- home/models.py
+++ home/models.py
@@ -14,7 +14,7 @@ class Portfolio(models.Model):
         (3,'브이로그'),
     )
     id = models.AutoField(primary_key = True) # 번호(화면에 안보임)
-    number = models.IntegerField(blank=True, null=True)  # 순서를 위한 필드
+    # number = models.IntegerField(blank=True, null=True)  # 순서를 위한 필드
     field = models.IntegerField(choices=FIELD_CHOICES)  # 필드
     title = models.CharField(max_length=255) # 제목
     description = models.CharField(max_length = 500) # 내용
@@ -23,15 +23,7 @@ class Portfolio(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) #등록일자
 
     class Meta:
-        ordering = ['number', 'created_at']  # number로 주로 정렬하되, 동일한 경우 created_at으로 정렬
-
-
-    def save(self, *args, **kwargs):
-        with transaction.atomic():
-            if self.number is None:
-                last_number = Portfolio.objects.select_for_update().aggregate(models.Max('number'))['number__max']
-                self.number = (last_number or 0) + 1
-            super().save(*args, **kwargs)
+        ordering = ['created_at']  # number로 주로 정렬하되, 동일한 경우 created_at으로 정렬
 
     def __str__(self):
         return self.title
--- home/views.py
+++ home/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.templatetags.static import static
 from home.models import Portfolio
 
 
 def index(request):
-    context = dict()
-    # 각 필드 값에 따라 분류
-    context['enterprise_portfolios'] = Portfolio.objects.filter(field=1)
-    context['product_portfolios'] = Portfolio.objects.filter(field=2)
-    context['vlog_portfolios'] = Portfolio.objects.filter(field=3)
+    # context = dict()
+    default_image = static('/home/images/loading_images.png')    # 각 필드 값에 따라 분류
+    context = {'enterprise_portfolios' : Portfolio.objects.filter(field=1),
+               'product_portfolios': Portfolio.objects.filter(field=2),
+               'vlog_portfolios' :Portfolio.objects.filter(field=3),
+               'default_image':default_image
+               }
+
+
+
+    # context['enterprise_portfolios'] = Portfolio.objects.filter(field=1)
+    # context['product_portfolios'] = Portfolio.objects.filter(field=2)
+    # context['vlog_portfolios'] = Portfolio.objects.filter(field=3)
 
     return render(request, "home/index.html", context)
